fd3e55ce5ad6d7720374ba88252c21f2b96e9a30
Nicholas Bentzen <bentzen.nich@gmail.com>
2019-01-12 22:54:50 -0600

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+let {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -25,6 +26,23 @@ app.get('/todos', (req, res) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+//GET /todos/12345
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    //valid id using isValid
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then( todo => {
+        if(!todo) return res.status(404).send();
+        res.send(todo);
+    }).catch( e => {
+        res.status(400).send();
     });
 });
 
