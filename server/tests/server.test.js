8ef6d4b6ae2ebe4b78546e24164aa238b8ecdbdd
Nicholas Bentzen <bentzen.nich@gmail.com>
2019-01-24 14:28:17 -0600

    updated expect to 1.20+

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -120,7 +120,7 @@ describe("DELETE /todos/:id", () => {
         if (err) return done(err);
         Todo.findById(hexId)
           .then(todo => {
-            expect(todo).toBeNull();
+            expect(todo).toBeFalsy();
             done();
           })
           .catch(e => done(e));
@@ -138,7 +138,7 @@ describe("DELETE /todos/:id", () => {
         if (err) return done(err);
         Todo.findById(hexId)
           .then(todo => {
-            expect(todo).not.toBeNull();
+            expect(todo).toBeTruthy();
             done();
           })
           .catch(e => done(e));
@@ -218,7 +218,7 @@ describe("PATCH /todos/:id", () => {
       .expect(res => {
         expect(res.body.todo.text).toBe(text);
         expect(res.body.todo.completed).toBe(false);
-        expect(res.body.todo.completedAt).toBeNull();
+        expect(res.body.todo.completedAt).toBeFalsy();
       })
       .end(done);
   });
@@ -257,8 +257,8 @@ describe("POST /users", () => {
       .send({ email, password })
       .expect(200)
       .expect(res => {
-        expect(res.headers["x-auth"]).toBeDefined();
-        expect(res.body._id).toBeDefined();
+        expect(res.headers["x-auth"]).toBeTruthy();
+        expect(res.body._id).toBeTruthy();
         expect(res.body.email).toBe(email);
       })
       .end(err => {
@@ -268,10 +268,8 @@ describe("POST /users", () => {
 
         User.findOne({ email })
           .then(user => {
-            expect(user).toBeDefined();
-            expect(user.password).toEqual(
-              expect.not.stringContaining(password)
-            );
+            expect(user).toBeTruthy();
+            expect(user.password).not.toBe(password);
             done();
           })
           .catch(e => done(e));
@@ -338,7 +336,7 @@ describe("POST /users/login", () => {
       })
       .expect(400)
       .expect(res => {
-        expect(res.headers["x-auth"]).toBeUndefined();
+        expect(res.headers["x-auth"]).toBeFalsy();
       })
       .end((err, res) => {
         if (err) return done(err);
